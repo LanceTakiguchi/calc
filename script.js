0aefccf38e85df060d6f344cae8be16fb544ee39
ltakiguc <ltakiguc@uci.edu>
2016-09-02 17:28:17 -0700

    save before I resort the way functions interact and operate

--- script.js
+++ script.js
@@ -9,50 +9,9 @@ Prompt: https://github.com/Learning-Fuze/calculator/tree/v1.0#getting-started
 var equation_string_array = [];
 var index = 0;
 var current_string = "";
-var last_button_operator;
-/* **display function: shows parameter into the calculator's display */
-var display = function(display_this){
-    /*console.log(display_this);*//*TODO: delete*/
-    $("#display").html(display_this);
-    console.log(equation_string_array)
-};
-/* **input_string: 1 parameters object with type, value. if it is a number, add to string, if operator, put into next index */
-var input_string = function(button){ /*TODO: create string array*/
-    if(button.type == "operator"){
-        last_button_operator = true;
-        index++;
-        equation_string_array[index] = button.value;
-        index++;
-        current_string = button.value;
-        if(button.value == "/"){
-            console.log("That's a divide");
-        }else if(button.value == "x"){
-            console.log("That's a multiply");
-        }else if(button.value == "-"){
-            console.log("That's a subtract");
-        }else{
-            console.log("That's addtion");
-        }
-    }else if(button.type == "clear"){
-        if (button.value == "c"){
-            console.log("That's a clear");
-        }else{
-            console.log("That's a clear all");
-        }
-    }else if(button.type == "decimal"){
-        console.log("That's a decimal");
-    }else if(button.type == "equals"){
-        console.log("that's an equals");
-    }else{
-        if(last_button_operator){ /* **This whole flag is to reset the current_string if it was last a operator */
-            last_button_operator = false;
-            current_string = "";
-        }
-        console.log("Its a number");
-        current_string += button.value;
-        equation_string_array[index] = current_string;
-    }
-};
+var last_button_operator; /* **Used to reset the current_string */
+var unsolved_operation; /* **Used to know when to do an operation */
+//* **Determines what kind of button was pushed */
 var input_type = function(button){
     if(button.value == "/" || button.value == "x" || button.value == "-" || button.value == "+"){
         return {type: "operator", value: button.value}
@@ -66,6 +25,54 @@ var input_type = function(button){
         return {type: "number", value: button.value}
     }
 };
+/* **display function: shows parameter into the calculator's display */
+var display = function(display_this){
+    /*console.log(display_this);*//*TODO: delete*/
+    $("#display").html(display_this);
+    console.log(equation_string_array)
+};
+/* **input_string: 1 parameters object with type, value. if it is a number, add to string, if operator, put into next index */
+var input_string = function(button){
+    if(button.type == "operator"){
+        last_button_operator = true;
+        unsolved_operation = true; /*TODO: make this usful*/
+        index++;
+        equation_string_array[index] = button.value;
+        index++;
+        if(button.value == "/"){
+            console.log("That's a divide");
+        }else if(button.value == "x"){
+            console.log("That's a multiply");
+        }else if(button.value == "-"){
+            console.log("That's a subtract");
+        }else{
+            console.log("That's addtion");
+        }
+    }else if(button.type == "clear"){
+        if (button.value == "ce"){
+            console.log("That's a clear");
+        }else{
+            console.log("That's a clear all");
+        }
+    }else if(button.type == "decimal"){
+        console.log("That's a decimal");
+    }else if(button.type == "equals"){
+        console.log("that's an equals");
+        /*TODO: Handel equals*/
+        /* ** Call appropriate operator function */
+    }else{
+        if(last_button_operator){ /* **This whole flag is to reset the current_string if it was last a operator */
+            last_button_operator = false;
+            current_string = "";
+        }
+        console.log("Its a number");
+        current_string += button.value;
+        equation_string_array[index] = current_string;
+    }
+};
+addition_operator = function (){
+    /*TODO*/
+}
 /* ** Run the js functions */
 $(document).ready(function(){
     $('.buttons button').on('click', function () {
