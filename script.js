56a0db453894be9dca934dee2c876ed042152eba
ltakiguc <ltakiguc@uci.edu>
2016-09-06 09:11:31 -0700

    Perfectly inputed number, add, number, equals works

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ Project Name: Calculator version 1.0
 File Name: script.js
 Author: Lance Takiguchi
 Date: 09/02/2016 Time: 10:42
+09/05/2016 16:00, 9/6/2016 09:07
 Objective: Aid in connect the calculator to the js to create functionality
-Prompt: https://github.com/Learning-Fuze/calculator/tree/v1.0#getting-started
+Prompt: https://github.com/Learning-Fuze/calculator/tree/v1#getting-started
 */
 var equation_string_array = [];
 var index = 0;
 var current_string = "";
 var last_button_operator; /* **Used to reset the current_string */
 var unsolved_operation; /* **Used to know when to do an operation */
+var solution; /* **the solution to the last operation */
 //* **Determines what kind of button was pushed */
 var determine_type = function(button){
     if(button.value == "/" || button.value == "x" || button.value == "-" || button.value == "+"){
@@ -32,6 +34,9 @@ var handle_type = function(button){
     if(button.type == "operator"){
         last_button_operator = true;
         unsolved_operation = true; /*TODO: make this usful*/
+        index++;
+        string_into_array(button);
+        index++;
         if(button.value == "/"){
             console.log("That's a divide");
         }else if(button.value == "x"){
@@ -39,7 +44,7 @@ var handle_type = function(button){
         }else if(button.value == "-"){
             console.log("That's a subtract");
         }else{
-            console.log("That's addtion");
+            console.log("That's addtion");;
         }
     }else if(button.type == "clear"){
         if (button.value == "ce"){
@@ -51,31 +56,19 @@ var handle_type = function(button){
         console.log("That's a decimal");
     }else if(button.type == "equals"){
         console.log("that's an equals");
+        solution = addition_operator();
+        display(solution);
         /*TODO: Handel equals*/
         /* ** Call appropriate operator function */
     }else{ /* If it was a number */ /*TODO: CURRENT*/
-        /*if(last_button_operator){ /!* **This whole flag is to reset the current_string if it was last a operator *!/
-            last_button_operator = false;
-            current_string = "";
-        }
-        console.log("Its a number");
-        current_string += button.value;
-        equation_string_array[index] = current_string;*/
+        string_into_array(button);
+        display(current_string);
     }
 }
-
-/* **display function: shows parameter into the calculator's display */
-var display = function(display_this){
-    /*console.log(display_this);*//*TODO: delete*/
-    $("#display").html(display_this);
-    console.log(equation_string_array)
-};
 /* **string_into_array: 1 parameters object with type, value. if it is a number, add to string, if operator, put into next index */
 var string_into_array = function(last_input){
     if(last_input.type == "operator"){
-        index++;
         equation_string_array[index] = last_input.value;
-        index++;
     }else{ /* **It is a number */
         if(last_button_operator){ /* **This whole flag is to reset the current_string if it was last a operator */
             last_button_operator = false;
@@ -86,14 +79,19 @@ var string_into_array = function(last_input){
         equation_string_array[index] = current_string;
     }
 };
+/* **display function: shows parameter into the calculator's display */
+var display = function(display_this){
+    /*console.log(display_this);*//*TODO: delete*/
+    $("#display").html(display_this);
+    console.log(equation_string_array)
+};
 addition_operator = function (){
-    /*TODO*/
+    return Number(equation_string_array[0]) + Number(equation_string_array[2]) /*TODO: Change from hardcoded to last two numbers*/
 }
 /* ** Run the js functions */
 $(document).ready(function(){
     $('.buttons button').on('click', function () {
         var button_input = $(this);
-        input_string(determine_type(button_input[0]));
-        display(current_string);
+        handle_type(determine_type(button_input[0]));
     });
 });
